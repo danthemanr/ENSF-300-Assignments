7e3b3344a754b7edc439bfce7aa6779628fdc7bc
danthemanr <danthemanr123@gmail.com>
2024-09-18 12:31:39 -0600

    version2-errorchecking

--- Calculator.py
+++ Calculator.py
@@ -25,7 +25,7 @@ def main():
         #print(usr_str)
 
         numstr_list = ["", "", ""]
-        op_list = [4, 4] #may need to be longer for brackets
+        op_list = [-1, -1, -1] #may need to be longer for brackets
         current = 0
         for x in usr_str:
             #print(x)
@@ -33,21 +33,23 @@ def main():
                 continue
             elif '0' <= x and x <= '9':
                 numstr_list[current] += x
-                print("a number in string format: ", numstr_list[current])
-            elif usr_str[0] == '-' and usr_str[1] == '-':
-                usr_str = "q"
-                break
+                #print("a number in string format: ", numstr_list[current])
             elif x in operators:
-                if x == '-' and (numstr_list[current] == ""): #If a number is negative
+                if x == '-' and (numstr_list[current] == "" or numstr_list[current][-1] == '-'): #If a number is negative
                     numstr_list[current] += x
-                    print("should be a dash: ", numstr_list[current])
+                    #print("should be a dash: ", numstr_list[current])
                 else:
                     op_list[current] = operators.index(x)
                     #print(op_list[current])
                     current += 1
+            elif op_list[2] != -1: #if there are too many operators
+                usr_str = "q"
+                character = x
+                break
             else:
                 usr_str = "q"
                 character = x
+                break
         if usr_str == "q":
             print('invalid format detected at character', character, 'program terminated')
             break
@@ -55,7 +57,7 @@ def main():
         num_list = [0, 0, 0]
         count = 0
         for s in numstr_list:
-            print("findnum: ", s)
+            #print("findnum: ", s)
             dec = 1
             for i in range(len(s)): #I could have used the int function
                 if s[-1-i] == '-':
@@ -63,7 +65,7 @@ def main():
                 else:
                     num_list[count] += (ord(s[-1-i]) - 48) * dec
                     dec *= 10
-            print("a number from num_list: ", num_list[count])
+            #print("a number from num_list: ", num_list[count])
             count += 1
         
         answer = 42 #default answer for testing
@@ -76,7 +78,7 @@ def main():
             #simple function
             temp = mathfunct(num_list[0], num_list[1], op_list[0])
             answer = mathfunct(temp, num_list[2], op_list[1])
-        print("first operation =", temp)
+        #print("first operation =", temp)
 
         print(usr_str, '=', answer)
         usr_str = input(prompt)
